ea69e90631422f94747a18cafe3f90a06965bf94
Prajwal <sharmaprajwal11@gmail.com>
2024-06-11 21:20:29 +0530

    created LinkedList

--- src/LinkedListQ.java
+++ src/LinkedListQ.java
@@ -1,3 +1,42 @@
 public class LinkedListQ {
-    
+
+    private ListNode head;
+
+    private class ListNode {
+
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void insertAtBeginning(int data) {
+        ListNode newNode = new ListNode(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void removeDuplicates() {
+        if (head == null) {
+            return;
+        }
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
